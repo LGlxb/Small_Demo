3f929f64ca726baaf2f194affb634587358c0fac
LGlxb <44798106+LGlxb@users.noreply.github.com>
2019-01-14 14:09:45 +0800

    0114commit

--- app/src/main/java/small/com/small_demo/app/SmallDemoApp.java
+++ app/src/main/java/small/com/small_demo/app/SmallDemoApp.java
@@ -1,13 +1,24 @@
 package small.com.small_demo.app;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 
+import small.com.small_demo.exceptionclass.MyBugly;
+
 public class SmallDemoApp extends Application {
+    private static Context context;
+
     @Override
     public void onCreate() {
         super.onCreate();
         Fresco.initialize(this);
+        MyBugly.getMyBugly().init(this);
+        context = this;
+    }
+
+    public static Context getContext() {
+        return context;
     }
 }
